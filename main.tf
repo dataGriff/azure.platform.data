8dac6bef18c1ce5fd8dc21273497c38e4f135ca0
RICHARD GRIFFITHS <griff182uk@yahoo.co.uk>
2023-05-14 16:49:18 +0100

    sorted key vault

--- main.tf
+++ main.tf
@@ -97,14 +97,14 @@ resource "azurerm_container_registry" "acr" {
   tags                = local.tags
 }
 
-# resource "azurerm_key_vault" "kv" {
-#   name                        = local.key_vault_name
-#   location                    = azurerm_resource_group.rg.name
-#   resource_group_name         = azurerm_resource_group.rg.location
-#   enabled_for_disk_encryption = true
-#   tenant_id                   = data.azurerm_client_config.current.tenant_id
-#   soft_delete_retention_days  = 7
-#   purge_protection_enabled    = false
-#   sku_name                    = "standard"
-#   tags                        = local.tags
-# }
+resource "azurerm_key_vault" "kv" {
+  name                        = local.key_vault_name
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name                    = "standard"
+  tags                        = local.tags
+}
